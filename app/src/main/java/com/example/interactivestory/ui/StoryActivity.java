9f4aa4f435e09d55ca95d462b833b0f15d34344c
OshedheMunasinghe <oshedhe91@gmail.com>
2020-05-10 18:12:40 +0200

    finished my first android app from treehouse!

--- app/src/main/java/com/example/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/example/interactivestory/ui/StoryActivity.java
@@ -11,10 +11,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.interactivestory.R;
 import com.example.interactivestory.model.Page;
 import com.example.interactivestory.model.Story;
+
+import java.util.Stack;
 
 public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
@@ -24,8 +27,7 @@ public class StoryActivity extends AppCompatActivity {
     private Button choice1Button;
     private Button choice2Button;
     private String name;
-
-
+    private Stack<Integer> pageStack = new Stack<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +51,8 @@ public class StoryActivity extends AppCompatActivity {
     }//end onCreate
 
     private void loadpage(int pageNumber) {
-    final Page page = story.getPage(pageNumber);
+        pageStack.push(pageNumber); //this is going to build up a stack of the pages as we go through them
+        final Page page = story.getPage(pageNumber);
         Drawable image = ContextCompat.getDrawable(this, page.getImageID());
         storyImageView.setImageDrawable(image);
         String pageText = getString(page.getTextID());
@@ -62,11 +65,10 @@ public class StoryActivity extends AppCompatActivity {
             choice2Button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                 //   finish(); //After end it will go to start
+                    //   finish(); //After end it will go to start
                     loadpage(0);
                 }
             });
-
         } else {
             loadButtons(page);
         }//end else if final page...
@@ -82,7 +84,6 @@ public class StoryActivity extends AppCompatActivity {
                 loadpage(nextPage);
             }
         });// choice1ButtonMethod
-
         choice2Button.setVisibility(View.VISIBLE);
         choice2Button.setText(page.getChoice2().getTextID());
         choice2Button.setOnClickListener(new View.OnClickListener() {
@@ -92,5 +93,18 @@ public class StoryActivity extends AppCompatActivity {
                 loadpage(nextPage);
             }
         });//end choice2buttonMethod
-    }
+    }//end loadButtons
+
+    //pop the stack eac htime the user taps on the back button
+
+    @Override
+    public void onBackPressed() {
+        //it will pop or exit the current activity and go bac kto main activity, no matter which page of the story we are on
+        pageStack.pop();
+        if(pageStack.isEmpty()) {
+            super.onBackPressed();
+        }else {
+            loadpage(pageStack.pop());
+        }
+    }//end onBackPressed
 }// end class
